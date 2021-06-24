2c2e9a9752361f27aeb15c0f452e84b036b12dce
avaz-dev <avaz.dev@icloud.com>
2021-06-24 23:50:10 +0500

    Add modal

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 var  elModalButton = document.querySelector('.header__link');
 var  elModalOpen = document.querySelector('.site-modal');
+var elModalClose = document.querySelector('.modal__button--close')
 
 
 elModalButton.addEventListener('click', function() {
-  elModalOpen.classList.toggle('site-modal--open');
+  elModalOpen.classList.add('site-modal--open');
+});
+
+elModalClose.addEventListener('click', function() {
+  elModalOpen.classList.remove('site-modal--open');
 });
